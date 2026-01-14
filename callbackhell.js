74f039036144bcf478439d07ef1ce8e480e5aadc
Blandus1 <blandine0045@gmail.com>
2026-01-14 18:54:03 +0200

    making a callback hell

---
+++ callbackhell.js
@@ -0,0 +1,28 @@
+const legacyAPI = {
+    login: (username, callback) => {
+        console.log("🔑 Logging in...");
+        setTimeout(() => callback(null, { userId: 42, user: username }), 1000);
+    },
+    getMovies: (userId, callback) => {
+        console.log(`📂 Fetching movies for User ID: ${userId}...`);
+        setTimeout(() => callback(null, ["Inception", "The Matrix", "Interstellar"]), 1000);
+    },
+    getShowtimes: (movieName, callback) => {
+        console.log(`🕒 Getting showtimes for: ${movieName}...`);
+        setTimeout(() => callback(null, ["7:00 PM", "9:30 PM", "11:00 PM"]), 1000);
+    },
+    bookTicket: (time, callback) => {
+        console.log(`🎟️ Booking ticket for ${time}...`);
+        setTimeout(() => callback(null, { success: true, seat: "F-14" }), 1000);
+    }
+};
+
+legacyAPI.login('Blandine',(error,data)=>{
+    legacyAPI.getMovies(data.userId,(error,movieArr)=>{
+        legacyAPI.getShowtimes(movieArr[0],(error,timeArr)=>{
+            legacyAPI.bookTicket(timeArr[0],(error,seat)=>{
+                console.log(`Success! Booked seat ${seat.seat} at ${timeArr[0]}.`)
+            })
+        })
+    })
+})
