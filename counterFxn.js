95f2ad0a71ecbe9370b3c31c30fd631ed273dcdf
Blandus1 <blandine0045@gmail.com>
2026-02-18 10:40:04 +0200

    the counter fxn with delay

--- counterFxn.js
+++ counterFxn.js
@@ -1,10 +1,37 @@
-//counting from 1 to 5 with a delay of 1 second in between
+
+//Task
+/*Create a counter function that counts from 1 to 5, with a 1-second delay between each number. The function should return a promise that resolves with an array of all the numbers when counting is complete. This tests your ability to create async flows with timing and collect results over time.
+
+Requirements:
+
+- Function `asyncCounter()` counts 1, 2, 3, 4, 5
+- 1-second delay between each number
+- Return promise that resolves with `[1, 2, 3, 4, 5]`
+- Use async/await or promise chaining
+
+*/
+
+//answer
 function asyncCounter(){
-let count = 1;
-for (let i=0; i< count; i++){
- setTimeout(() => {
+  return new Promise((resolve,reject)=>{
+     let arr=[]
+     let i=1
+     function count(){
+       if(i<=5){
+           setTimeout(()=>{
+     arr.push(i)
+     console.log(i)
+             i++;
+             count();
+    },1000);
+       }else{
+         resolve(arr)
+       }
     
- },1000);
+     }
+   count()
+  })
 }
- 
-}
+
+asyncCounter().then(res=>console.log(res))
+.catch(error=>console.log(error))
